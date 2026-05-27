ec9ff05892e47559b35dae3bc1305d244ad62e22
Iván Bautista <ivanbautistag@gmail.com>
2026-05-26 19:37:36 -0600

    feat: add test template API endpoint

--- api/template.ts
+++ api/template.ts
@@ -1,9 +1,10 @@
-import type { VercelRequest, VercelResponse } from "@vercel/node";
-import { reasoningGenerateTemplate } from "../src/lib/reasoning.js";
+/*import type { VercelRequest, VercelResponse } from "@vercel/node";
+import { generateTemplates } from "../src/lib/reasoning.js";
 import { sendAllMessages } from "../src/lib/notifier.js";
 
 export default async function handler(req: VercelRequest, res: VercelResponse) {
-  const templateMessages = await reasoningGenerateTemplate();
+  const templateMessages = await generateTemplates();
   await sendAllMessages(templateMessages);
   res.json(templateMessages);
 }
+*/
